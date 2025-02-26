f7da93bed5db59ef782a3bcedd718423cc8133a4
NK_31 <102040326+nk-31012002@users.noreply.github.com>
2025-02-26 19:50:11 +0530

    Grid

--- codeBase.js
+++ codeBase.js
@@ -2,3 +2,13 @@ function setup() {
   createCanvas(400, 400);
   frameRate(30);
 }
+
+function drawGrid() {
+  let w = width / 3;
+  let h = height / 3;
+  strokeWeight(4);
+  line(w, 0, w, height);
+  line(w * 2, 0, w * 2, height);
+  line(0, h, width, h);
+  line(0, h * 2, width, h * 2);
+}
