d1502e6abe74c72d77bf7b70594f01aa22c890a4
NK_31 <102040326+nk-31012002@users.noreply.github.com>
2025-02-26 19:52:53 +0530

    Draw Player Moves

--- codeBase.js
+++ codeBase.js
@@ -21,3 +21,25 @@ let board = [
 
 let players = ['X', 'O'];
 let currentPlayer = 0;
+
+
+function drawMarks() {
+  let w = width / 3;
+  let h = height / 3;
+  for (let j = 0; j < 3; j++) {
+    for (let i = 0; i < 3; i++) {
+      let x = w * i + w / 2;
+      let y = h * j + h / 2;
+      let spot = board[j][i];
+      textSize(32);
+      if (spot == players[1]) {
+        noFill();
+        ellipse(x, y, w / 2);
+      } else if (spot == players[0]) {
+        let xr = w / 4;
+        line(x - xr, y - xr, x + xr, y + xr);
+        line(x + xr, y - xr, x - xr, y + xr);
+      }
+    }
+  }
+}
